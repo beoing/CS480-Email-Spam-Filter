eeabbbdaad9ad53915d6cecdcfe4de5eb734420d
beoing <christopher.c.to@gmail.com>
2015-11-23 23:54:48 -0600

    Added some commands to main for easier running.

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from spamfilter import *
 
 mySpamFilter = SpamFilter()
 mySpamFilter.bulkTrain()
+#mySpamFilter.bulkTrain("../spamEnron", "../hamEnron")
 mySpamFilter.bulkClassify()
-# large corpuses
-#mySpamFilter.bulkTrain("./spamEnron", "./hamEnron")
-#mySpamFilter.bulkClassify("./evaluationEnron")
+#mySpamFilter.bulkClassify("./evaluationLing")
 
 
