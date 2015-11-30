c3dd640c9af4acde546a9c963f7cec3f4a86f684
beoing <christopher.c.to@gmail.com>
2015-11-29 18:24:06 -0600

    Added notes to main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+# to run: create the object, bulk train it, then classify a directory
+# by default parameters, it uses the ./spam, ./ham, and ./evaluation
+# directories, but you can specify your own.  
 
 from spamfilter import *
 
 sf = SpamFilter()
 #sf.bulkTrain()
-#sf.bulkTrain("./test","./test")
-sf.bulkTrain("../spamEnron", "../hamEnron")
+#sf.bulkTrain("../spamEnron", "../hamEnron")
 #sf.bulkTrain("../spamLing2","../hamLing2")
 #sf.bulkTrain("../spamLing","../hamLing")
-#sf.bulkClassify()
-sf.bulkClassify("./evaluationLing")
-
-
+sf.bulkClassify()
+#sf.bulkClassify("./evaluationLing")
+sf.bulkClassify()
